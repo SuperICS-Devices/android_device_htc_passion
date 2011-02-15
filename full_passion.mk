3190f03f687e97bd22334963a1c1a7b86b94ba73
macsa <macsa@mac-sa.es>
2011-02-15 16:25:58 +0100

    Cambio para Europeo
    Change-Id: I64bd784402732a70b2c56e89993f19635161387f

--- full_passion.mk
+++ full_passion.mk
@@ -23,7 +23,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
-$(call inherit-product, device/htc/passion/passion_us.mk)
+$(call inherit-product, device/htc/passion/passion_eu.mk)
 
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_passion
