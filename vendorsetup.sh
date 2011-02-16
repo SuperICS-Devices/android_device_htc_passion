58103afda37d68dbad66be3200eb8c9737f06920
macsa <macsa@mac-sa.es>
2011-02-16 16:26:25 +0100

    Limpieza
    Change-Id: I559a10830cedfffae66e9b4305f4f53606063ac2

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,6 +20,6 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo full_passion-userdebug
-add_lunch_combo full_passion-eng
+#add_lunch_combo full_passion-userdebug
+#add_lunch_combo full_passion-eng
 
