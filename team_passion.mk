c18ea3fdb344830905e5ea94c7304e2fdf6afcf3
nelo360 <nelo360@gmail.com>
2012-04-03 13:34:48 +0200

    Not buid display ID, fixed paths, added product name
    Change-Id: I5b25b7db398fd96d5794c3b95ce8cad0ed644112

--- team_passion.mk
+++ team_passion.mk
@@ -1,8 +1,12 @@
-$(call inherit-product, vendor/SuperTeam/products/high_telephony_device.mk)
+# Release name
+PRODUCT_RELEASE_NAME := NexusOne
 
-$(call inherit-product, vendor/SuperTeam/products/flash.mk)
 
-$(call inherit-product, vendor/SuperTeam/products/hdpi.mk)
+$(call inherit-product, vendor/osr/config/gsm.mk)
+
+$(call inherit-product, vendor/osr/config/asian.mk)
+
+$(call inherit-product, vendor/osr/config/common_full_phone.mk)
 
 $(call inherit-product, device/htc/passion/passion.mk)
 
@@ -14,7 +18,7 @@ PRODUCT_BRAND := google
 PRODUCT_DEVICE := passion
 PRODUCT_MODEL := Nexus One
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=passion BUILD_ID=GRJ22 BUILD_DISPLAY_ID=GWK74 BUILD_FINGERPRINT=google/passion/passion:2.3.4/GRJ22/121341:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.4 GRJ22 121341 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=passion BUILD_ID=GRJ22 BUILD_FINGERPRINT=google/passion/passion:2.3.4/GRJ22/121341:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.4 GRJ22 121341 release-keys"
 
 # Build kernel
 #PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL=
@@ -22,12 +26,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=passion BUILD_ID=GRJ22 BUILD_DISPLA
 #PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_CONFIG=aosp_HAVS_mahimahi_defconfig
 
 # Extra Passion overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/SuperTeam/overlay/passion
+PRODUCT_PACKAGE_OVERLAYS += vendor/osr/overlay/passion
 
 # Extra RIL settings
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.ril.enable.managed.roaming=1 \
     ro.ril.oem.nosim.ecclist=911,112,999,000,08,118,120,122,110,119,995 \
     ro.ril.emc.mode=2
-
-PRODUCT_VERSION_MAINTENANCE := 9999
