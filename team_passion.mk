ee423f63d4a1bbdd30162f68da19e8e0e75fcf93
nelo360 <nelo360@gmail.com>
2012-04-13 18:38:32 +0200

    updated passion calls to the new dirs
    Change-Id: I9f5369b32a2796e44760778b85ae609b70805b1d

--- team_passion.mk
+++ team_passion.mk
@@ -2,11 +2,11 @@
 PRODUCT_RELEASE_NAME := NexusOne
 
 
-$(call inherit-product, vendor/osr/config/gsm.mk)
+$(call inherit-product, vendor/osr/products/asian.mk)
 
-$(call inherit-product, vendor/osr/config/asian.mk)
+$(call inherit-product, vendor/osr/products/high_telephony_device.mk)
 
-$(call inherit-product, vendor/osr/config/common_full_phone.mk)
+$(call inherit-product, vendor/osr/products/hdpi.mk)
 
 $(call inherit-product, device/htc/passion/passion.mk)
 
@@ -26,10 +26,19 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=passion BUILD_ID=GRJ22 BUILD_FINGER
 #PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_CONFIG=aosp_HAVS_mahimahi_defconfig
 
 # Extra Passion overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/osr/overlay/passion
+PRODUCT_PACKAGE_OVERLAYS += device/htc/passion-common/overlay
 
 # Extra RIL settings
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.ril.enable.managed.roaming=1 \
     ro.ril.oem.nosim.ecclist=911,112,999,000,08,118,120,122,110,119,995 \
     ro.ril.emc.mode=2
+
+# Inherit Flash Player	
+-include vendor/osr/products/flashplayer.mk
+
+# Inherit drm blobs
+#-include vendor/osr/products/drm.mk
+
+# Inherit Gapps blobs
+#-include vendor/osr/products/common_gapps.mk
